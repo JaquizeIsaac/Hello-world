e9422986b8d6b300861d95fa0527730deffdedb7
Myyel Isaac <147030259+JaquizeIsaac@users.noreply.github.com>
2023-10-13 14:57:47 -0700

    updating website

--- app.js
+++ app.js
@@ -14,15 +14,3 @@ if (age && email && name) {
   alert('You did not provide all the requested information.');
 }
 
-function askNumberOfPairs() {
-    const numberOfPairs = prompt("How many pairs of shoes do you own?");
-    const resultElement = document.getElementById("result");
-    const errorMessageElement = document.getElementById("error-message");
-
-    if (numberOfPairs === null || numberOfPairs === "") {
-        errorMessageElement.textContent = "Please enter a valid number of pairs.";
-        resultElement.textContent = "";
-    } else if (isNaN(numberOfPairs) || Number(numberOfPairs) < 0) {
-        errorMessageElement.textContent = "Please enter a valid positive number of pairs.";
-        resultElement.textContent = "";
-    }
