79ab6e4fca4e881178b06f364fd8e6a38b75e04d
Myyel Isaac <147030259+JaquizeIsaac@users.noreply.github.com>
2023-10-13 14:17:27 -0700

    updating webpage

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-use strict'
+// Ask for the user's age
+const age = prompt('How old are you?');
 
-let username = prompt('What is your name?');
-console.log('Thank you visting, ' + username);
+// Ask for the user's email
+const email = prompt('What is your email address?');
 
+// Ask for the user's name
+const name = prompt('What is your name?');
+
+// Display the collected information
+if (age && email && name) {
+  alert(`Thank you, ${name}! You are ${age} years old, and your email is ${email}.`);
+} else {
+  alert('You did not provide all the requested information.');
+}
