088d53737183ce4d4c7efc60a7232abdcdc1bac3
Myyel Isaac <147030259+JaquizeIsaac@users.noreply.github.com>
2023-10-13 13:43:16 -0700

    updating webpage

--- app.js
+++ app.js
@@ -3,16 +3,3 @@ use strict'
 let username = prompt('What is your name?');
 console.log('Thank you visting, ' + username);
 
-let age = prompt('How old are you? (15-100)');
-let message; 
-
-let email = prompt('Enter your email to get notified on new customs');
-let message;
-
-if(age <= 15){
-    message = 'Cool, check out the new custom drops every Saturday';
-} else if (age >=14 ){
-    message = 'maybe next time';}
-
-// look for the "<h2><script src="app.js"></script></h2>" on line 20 in the HTML, this is where the message will write too!
-document.write('Hello ' + username + '! ' + message);
